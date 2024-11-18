98a7c3dd5e2c6506982ce7d3d1f002c70b005635
carolsonggg <107651075+carolsonggg@users.noreply.github.com>
2024-11-18 21:12:39 +0000

    new file:   proposal new file:   src/game.py new file:   src/graphics.py new file:   src/input.py new file:   src/main.py

---
+++ src/input.py
@@ -0,0 +1,2 @@
+#manages user interactions like mouse clicks, drag-and-drop for toppings, and navigating the order screen
+from cmu_graphics import *
---
+++ src/main.py
@@ -0,0 +1,9 @@
+#initialize the game, load necessary assets, and call functions to run the game loop.
+from cmu_graphics import *
+
+WIDTH, HEIGHT = 800, 600
+app = App(width=WIDTH, height=HEIGHT)
+
+def main():
+    # main game loop?
+    app.run()
